5dbee88b29c5cfb61d4331730bbd083d4acc11b2
Ashutosh Pandey <158585140+Ashutoshpandey3110@users.noreply.github.com>
2025-08-07 16:57:02 +0530

    changes in start_container

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,6 +4,6 @@ set -e
 echo "Pulling latest image and starting container..."
 
 docker pull ashu32/simple-python-flask-app
-docker run -d --name flaskapp -p 5000:5000 ashu32/simple-python-flask-app
+docker run -d -p 5000:5000 ashu32/simple-python-flask-app
 
 echo "Container started successfully."
